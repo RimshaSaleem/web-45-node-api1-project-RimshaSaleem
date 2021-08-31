0ea552c42fde33d110bcf7824f433759abebc6b0
RimshaSaleem <rimshatif8@gmail.com>
2021-08-30 22:06:35 -0700

    done 2 test

--- api/server.js
+++ api/server.js
@@ -1,3 +1,38 @@
 // BUILD YOUR SERVER HERE
-
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express')
+const User = require('./users/model')
+const server = express()
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        console.log(users)
+        res.json(users)
+        // res.status(200).json(dogs)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({ 
+            message: 'error getting users',
+            err: err.message })
+      })
+  })
+  server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if(!user){
+            res.status(404).json({
+            message: "The user with the specified ID does not exist",
+         })
+        }
+         console.log(user)
+        res.json(user)
+        // res.status(200).json(dogs)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({ 
+            message: 'error getting user',
+            err: err.message })
+      })
+  })
--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
-console.log(' server is listening on port 5000')
+// console.log(' server is listening on port 5000')
+server.listen(port, () => { // only one program can hog 5000 at a time
+    console.log(' server is listening on port' ,port) // success callback
+  })
+  
